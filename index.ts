db4b25731c6d3e6755f0c80a1b9af08d36fc134c
FaizAhmed55 <159883350+FaizAhmed55@users.noreply.github.com>
2024-04-21 22:17:20 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,76 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let mybalance = 20000;
+let mypin = 1111;
+
+let pinanswer = await inquirer.prompt(
+    [
+  {
+    name: "pin",
+    message: "enter your pin",
+    type: "number",
+  },
+]
+);
+
+if (pinanswer.pin === mypin){
+  console.log("Correct Your pin !!!");
+
+  let operationans = await inquirer.prompt(
+    [
+        {
+      name: "operation",
+      message: "select your option",
+      type: "list",
+      choices: ["withdrawl","fast Cash","checkbalance"],
+        }
+    ]
+);
+
+  if (operationans.operation === "withdrawl"){
+    let amountans = await inquirer.prompt(
+        [
+      {
+        name: "amount",
+        message: "enter your withdrawl amount",
+        type: "number",
+      }
+    ]
+);
+
+    if(amountans.amount > mybalance)(
+        console.log("Insufficent Balance")
+    );
+
+    else{
+       (mybalance -= amountans.amount)
+    console.log(`Your Remaining Balance Is ${mybalance}`)
+}
+
+};
+
+if (operationans.operation === "fast Cash"){
+    let fast = await inquirer.prompt(
+        [
+      {
+        name: "cash",
+        message: "select the amount which you withdrawl",
+        type: "list",
+        choices:[1000,2000,3000,5000]
+      }
+    ]
+);
+
+mybalance -= fast.cash;
+console.log(`you have successfully withdrawl ${fast.cash} \n Your Remaining is ${mybalance}`)
+
+}
+
+
+else if( operationans.operation === "checkbalance")
+    console.log(`your Balance Is ${mybalance}`)
+}
+else{
+  console.log("Incorrect Your Pin")
+};
